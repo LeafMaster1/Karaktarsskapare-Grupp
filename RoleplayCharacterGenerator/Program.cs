fe34bf9beb9531ce2c7255c0c15143f2258b5118
Viktor Johansson <discovicke@gmail.com>
2026-09-22 13:28:09 +0200

    Chrille in bikini in Marbella, Chrille in bikini in Kairo

--- RoleplayCharacterGenerator/Program.cs
+++ RoleplayCharacterGenerator/Program.cs
@@ -1,6 +1,19 @@
 using DotNetEnv;
+using Azure;
+using OpenAI;
+using OpenAI.Images;
+using System;
+using System.IO;
+using System.ClientModel;
+
+#pragma warning disable OPENAI001
+
 Env.Load();
 var builder = WebApplication.CreateBuilder(args);
+const string endpoint = "https://llmresurs.services.ai.azure.com/openai/v1";
+const string deploymentName = "gpt-image-1.5";
+string apiKey = Environment.GetEnvironmentVariable("AZURE_OPENAI_KEY") 
+                ?? throw new InvalidOperationException("AZURE_OPENAI_KEY environment variable is not set.");
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -24,5 +37,26 @@ app.UseAuthorization();
 app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
+
+ImageClient client = new(
+    credential: new ApiKeyCredential(apiKey),
+    model: deploymentName,
+    options: new OpenAIClientOptions()
+    {
+        Endpoint = new($"{endpoint}"),
+    }
+);
+
+string prompt = "A cute baby polar bear";
+
+ImageGenerationOptions options = new()
+{   
+    Size = GeneratedImageSize.W1024xH1024,
+};
+
+GeneratedImage image = client.GenerateImage(prompt, options);
+BinaryData bytes = image.ImageBytes;
+
+File.WriteAllBytes("output.png", bytes.ToArray());
 
 app.Run();
